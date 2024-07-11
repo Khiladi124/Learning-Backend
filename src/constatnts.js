2d58cc59c3c9c3f8efce3358df103ba4a15dc0c9
abhi <abhi.badam186@gmail.com>
2024-07-11 22:36:17 +0530

    connect mongoDb databse

---
+++ src/constatnts.js
@@ -0,0 +1,1 @@
+export const DB_NAME="MP4tube"
---
+++ src/index.js
@@ -0,0 +1,34 @@
+//require('dotenv').config({path: './env'});
+import dotenv from 'dotenv';
+import mongoose from 'mongoose'
+import { DB_NAME } from './constatnts.js';
+import connectDB from './db/index.js';
+import express from "express";
+
+dotenv.config({
+    path: './env'
+});
+
+// and then do in script "dev":"nodemon -r dotenv/config --experimental-json-modules src/index.js"
+connectDB();
+
+
+/*
+;(async()=>{
+    try{
+     const connect=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+     application.on("error",(error)=>{
+        console.log("ERR: ", error);
+        throw error;
+     });
+
+     app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+     })
+    }catch(error){
+        console.errot("ERROR:",error)
+        throw error;
+    }
+})()
+
+*/
